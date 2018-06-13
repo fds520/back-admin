4b73c88090c4d5dfc66dfeacd891ceff10f98334
大双mmd <2296005140@qq.com>
2018-06-13 22:02:26 +0800

    添加自定义登录

---
+++ src/app/pages/login/login.register.module.ts
@@ -0,0 +1,26 @@
+/*
+import { NgModule } from '@angular/core';
+
+import { TreeModule } from 'ng2-tree';
+import { ToasterModule } from 'angular2-toaster';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { ComponentsRoutingModule, routedComponents } from './components-routing.module';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+@NgModule({
+  imports: [
+    ThemeModule,
+    ComponentsRoutingModule,
+    TreeModule,
+    ToasterModule,
+    NgZorroAntdModule
+  ],
+  declarations: [
+    ...routedComponents
+  ],
+  exports: [
+    NgZorroAntdModule
+  ]
+})
+export class LoginRegisterModule { }
+*/
