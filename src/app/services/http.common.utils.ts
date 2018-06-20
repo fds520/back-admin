2982f551a1950fc530467f8e2aac54b466a428b2
大双mmd <2296005140@qq.com>
2018-06-20 23:08:40 +0800

    Authorization请求头飞控判断

--- src/app/services/http.common.utils.ts
+++ src/app/services/http.common.utils.ts
@@ -14,7 +14,6 @@ export class HttpCommonUtils {
               private router: Router) {
     // 请求头配置
     this.headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    this.headers = this.headers.set('Authorization', '123');
   }
 
   /**
@@ -24,6 +23,9 @@ export class HttpCommonUtils {
    * @returns {any}
    */
   get(url: string, params?: any): any {
+    if (this.storageUtils.getLocalStorage('token')) {
+      this.headers = this.headers.set('Authorization', this.storageUtils.getLocalStorage('token'));
+    }
     return this.intercept(this.http.get(url, {headers: this.headers, params: params}));
   }
 
@@ -34,6 +36,9 @@ export class HttpCommonUtils {
    * @returns {any}
    */
   post(url: string, body): any {
+    if (this.storageUtils.getLocalStorage('token')) {
+      this.headers = this.headers.set('Authorization', this.storageUtils.getLocalStorage('token'));
+    }
     return this.intercept(this.http.post(url, this.transformRequest(body),
       {headers: this.headers}));
   }
@@ -56,12 +61,15 @@ export class HttpCommonUtils {
       observable.subscribe(res => {
         const code = res.code;
         console.info(res);
-        if (code === '10000') {
+        if (code === 10000) {
           // 请求验证通过
           observer.next(res);
-        } else if (code === '10001') {
+        } else if (code === 10001) {
           console.info('参数错误 1确定刷新页面，取消返回');
-        } else if (code === '10002') {
+        } else if (code === 10002) {
+
+          // 删除token
+          this.storageUtils.removeLocalStorage('token');
           console.info('token非法；重新登陆');
           this.router.navigate(['/auth/login']);
         } else {
